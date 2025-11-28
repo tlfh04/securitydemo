fa8b2f631fa9afc7d0afa3032ade823a038ce54a
tlfh04 <126224780+tlfh04@users.noreply.github.com>
2025-11-28 11:47:53 +0900

    UserDetails 커스터마이징

---
+++ src/main/java/com/example/securitydemo/entity/User.java
@@ -0,0 +1,32 @@
+package com.example.securitydemo.entity;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="users")
+@Getter
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false,unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+
+    @Builder
+    public User(String username, String password, String role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+}
